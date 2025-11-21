00155e39e9df2b529973aa2d0b79b821d1bc12e8
Lucas Melo <164279989+LucasAtMelo@users.noreply.github.com>
2025-11-21 19:15:16 -0300

    exercicio 1 respondido

---
+++ exercicios/exercicio1_verificacao-idade.py
@@ -0,0 +1,11 @@
+idade = int(input('Digite sua idade: ')) #Atribui a idade do usuário a uma variavel 
+
+#se o usuário for maior de idade 
+if idade >= 18:
+    # a entrada é permitida 
+    print('Entrada permitida ao evento.')
+
+#se não 
+else: 
+    #entrada negada 
+    print('Entrada não permitida ao evento. ')
